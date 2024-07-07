b4f36f5b486b710ae33a4af8849299ddf77ce16c
NazarDushka <dushkanazar@gmail.com>
2024-07-07 12:37:46 +0300

    Calculator controller added

---
+++ WebApplication1/WebApplication1/Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GreetCalc(decimal num1, decimal num2, char key)
+        {
+            string result;
+
+            switch (key)
+            {
+                case '+':
+                    result = $"Sum = {num1 + num2}";
+                    break;
+                case '-':
+                    result = $"Subtract = {num1 - num2}";
+                    break;
+                case '*':
+                    result = $"Multiply = {num1 * num2}";
+                    break;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        return BadRequest("Division by zero is not allowed.");
+                    }
+                    result = $"Divide = {num1 / num2}";
+                    break;
+                default:
+                    return BadRequest("Invalid operation key.");
+            }
+
+            return Ok(result);
+        }
+    }
+}
